19202f4e0ff538224bc38fc4e56d7b9458f86f93
FarahAppExpert <farah.aljoubori@appexperts.me.uk>
2022-04-26 13:13:13 +0100

    work

---
+++ app/src/main/java/com/example/mvvmpractice/UI/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.mvvmpractice.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mvvmpractice.R
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
